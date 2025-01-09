0b8a9948e4d12854ada78c03346ed0a8648df681
Adam Kliment <adam@netmilk.net>
2025-01-09 13:18:55 +0100

    Fixed dataset schema and removed debug from Actor Dockerfile

--- .actor/actor.sh
+++ .actor/actor.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-WORKDIR="/opt/sherlock/"
 INPUT=`apify actor:get-input | jq -r .usernames[] | xargs echo`
 echo "INPUT: $INPUT"
-
-cd $WORKDIR
 
 sherlock $INPUT
 
 for username in $INPUT; do
+  # escape the special meaning leading characters 
+  # https://github.com/jpmens/jo/blob/master/jo.md#description
+  safe_username=$(echo $username | sed 's/^@/\\@/' | sed 's/^:/\\:/' | sed 's/%/\\%/')
   echo "pushing results for username: $username"
-  sed '$d' $username.txt | jo -a | jo username=$username links:=- | apify actor:push-data
+  sed '$d' $username.txt | jo -a | jo username=$safe_username links:=- | apify actor:push-data
 done
-
-
