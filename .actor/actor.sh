78a445622ae54792eccb3d8991e582351eda94c2
Adam Kliment <adam@netmilk.net>
2025-03-17 12:56:48 +0100

    Update Readmes for Apify Actor

--- .actor/actor.sh
+++ .actor/actor.sh
@@ -8,6 +8,7 @@ for username in $INPUT; do
   # escape the special meaning leading characters 
   # https://github.com/jpmens/jo/blob/master/jo.md#description
   safe_username=$(echo $username | sed 's/^@/\\@/' | sed 's/^:/\\:/' | sed 's/%/\\%/')
-  echo "pushing results for username: $username"
+  echo "pushing results for username: $username, content:"
+  cat $username.txt
   sed '$d' $username.txt | jo -a | jo username=$safe_username links:=- | apify actor:push-data
 done
